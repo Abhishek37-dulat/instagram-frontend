b54778f453b4ff893c9ee24f06d59c7874134846
Abhishek37-dulat <abhishek11906997dulat@gmail.com>
2023-06-21 11:48:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./components/screens/Home";
+import Login from "./components/screens/Login";
+import Profile from "./components/screens/Profile";
+import Signup from "./components/screens/Signup";
+import CreatePost from "./components/screens/CreatePost";
+import { useEffect, useReducer, createContext, useContext } from "react";
+import { reducer, initialState } from "./reducers/userReducer";
+import { BrowserRouter } from "react-router-dom";
+
+export const UserContext = createContext();
+
+const Routing = () => {
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "USER", payload: user });
+    } else {
+      navigate("/login");
+    }
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" exact="true" element={<Home />} />
+      <Route path="/login" exact="true" element={<Login />} />
+      <Route path="/profile" exact="true" element={<Profile />} />
+      <Route path="/signup" exact="true" element={<Signup />} />
+      <Route path="/create" exact="true" element={<CreatePost />} />
+    </Routes>
+  );
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserContext.Provider value={{ state, dispatch }}>
+        <BrowserRouter>
+          <Navbar />
+          <Routing />
+        </BrowserRouter>
+      </UserContext.Provider>
+    </>
   );
 }
 
